12e02086af6dc11424ae27e367c34547199f90a6
unknown <badullah@roehampton.ac.uk>
2022-12-07 16:02:46 +0000

    database added

--- sd2-db.sql
+++ sd2-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 30, 2022 at 09:54 AM
--- Server version: 8.0.24
--- PHP Version: 7.4.20
+-- Generation Time: Dec 07, 2022 at 03:52 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,232 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_table`
+-- Table structure for table `guest`
 --
 
-CREATE TABLE `test_table` (
+CREATE TABLE `guest` (
   `id` int NOT NULL,
-  `name` varchar(512) NOT NULL
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `test_table`
+-- Dumping data for table `guest`
 --
 
-INSERT INTO `test_table` (`id`, `name`) VALUES
-(1, 'Lisa'),
-(2, 'Kimia');
+INSERT INTO `guest` (`id`, `name`) VALUES
+(1, 'Haseena'),
+(2, 'Andrea'),
+(3, 'Saad'),
+(4, 'Lisa'),
+(5, 'Kevin'),
+(6, 'Julia'),
+(7, 'Sammy'),
+(8, 'Aisha'),
+(9, 'David'),
+(10, 'Kerrin'),
+(11, 'Mary'),
+(12, 'Kate'),
+(13, 'Justin'),
+(14, 'Jackie'),
+(15, 'Carol'),
+(16, 'Marina'),
+(17, 'Noam'),
+(18, 'Ruth');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest_list`
+--
+
+CREATE TABLE `guest_list` (
+  `id` int DEFAULT NULL,
+  `list_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `guest_list`
+--
+
+INSERT INTO `guest_list` (`id`, `list_id`) VALUES
+(1, 1),
+(2, 3),
+(3, 2),
+(4, 1),
+(5, 3),
+(6, 1),
+(7, 3),
+(8, 3),
+(9, 1),
+(10, 1),
+(11, 2),
+(12, 2),
+(13, 1),
+(14, 1),
+(15, 3),
+(16, 3),
+(17, 2),
+(18, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `code` int NOT NULL,
+  `item_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`code`, `item_name`) VALUES
+(1, 'dinner set'),
+(2, 'coffeemaker'),
+(3, 'blender'),
+(4, 'saucepan'),
+(5, 'kettle'),
+(6, 'knife set'),
+(7, 'cutlery'),
+(8, 'wok'),
+(9, 'Pressure Cooker'),
+(10, 'Juicer'),
+(11, 'Memory Book'),
+(12, 'Travel Cot'),
+(13, 'Changing Table'),
+(14, 'Burp Cloth'),
+(15, 'Rocking Chair'),
+(16, 'Sound Machine'),
+(17, 'Baby Monitor'),
+(18, 'Baby Clothes'),
+(19, 'Toys'),
+(20, 'Blanket'),
+(21, 'Book'),
+(22, 'PS5'),
+(23, 'Oculus Rift'),
+(24, 'Doll'),
+(25, 'Darth Vader Bust'),
+(26, 'Lightsaber');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `list_id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `list`
+--
+
+INSERT INTO `list` (`list_id`, `name`, `type`, `password`) VALUES
+(1, 'Saad Birthday', 'birthday', 'password1'),
+(2, 'Amy Wedding', 'wedding', 'password2'),
+(3, 'Sarah Baby Shower', 'baby shower', 'password3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list_item`
+--
+
+CREATE TABLE `list_item` (
+  `id` int DEFAULT NULL,
+  `list_id` int DEFAULT NULL,
+  `code` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `list_item`
+--
+
+INSERT INTO `list_item` (`id`, `list_id`, `code`) VALUES
+(1, 1, 22),
+(2, 1, 23),
+(3, 1, 24),
+(4, 1, 25),
+(5, 1, 26),
+(6, 2, 1),
+(7, 2, 2),
+(8, 2, 3),
+(9, 2, 4),
+(10, 2, 5),
+(11, 2, 6),
+(12, 2, 7),
+(13, 2, 8),
+(14, 2, 9),
+(15, 2, 10),
+(16, 3, 12),
+(17, 3, 13),
+(18, 3, 14),
+(19, 3, 15),
+(20, 3, 16),
+(21, 3, 17),
+(22, 3, 18),
+(23, 3, 19),
+(24, 3, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`) VALUES
+(1, 'Ana', 'ana@email.com'),
+(2, 'Billy', 'billy@email.com'),
+(3, 'Charlie', 'charlie@email.com'),
+(4, 'Diana', 'diana@email.com'),
+(5, 'Eliana', 'eliana@email.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `test_table`
+-- Indexes for table `guest`
 --
-ALTER TABLE `test_table`
+ALTER TABLE `guest`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `item`
 --
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`code`);
 
 --
--- AUTO_INCREMENT for table `test_table`
+-- Indexes for table `list`
 --
-ALTER TABLE `test_table`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`list_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
